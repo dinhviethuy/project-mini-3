1d2c0e896af986f98a3851ae8ac43c526a874d89
Đinh Viết Huy <154805218+dinhviethuy@users.noreply.github.com>
2024-08-20 22:09:25 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,3 @@
-export var PATHNAME='http://localhost:3000';
+export var PATHNAME='https://8x6qsp-3000.csb.app';
 export var PRODUCT_API = `${PATHNAME}/products`;
 export var CATEGORY_API = `${PATHNAME}/category`;
